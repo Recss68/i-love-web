a5fc4de5a03b93df570b4c2ef52b790f68f5dd7d
Recep <recep.yildiz@hva.nl>
2024-12-13 01:59:19 +0100

    basis opgesteld van i love web
    basis opgesteld van i love web

--- index.js
+++ index.js
@@ -1,19 +1,1 @@
-// Haalt de button element op door te zoeken naar ID | Maak de click function
-document.querySelector('.toggleButton').addEventListener('click', function () {
-    // Haalt de text element en classes op
-    let verborgenText = document.querySelector('.verborgen, .showing-more');
-    
-    // Toggle de zichtbaarheid van de tekst door classes te wisselen
-    if (verborgenText.classList.contains('verborgen')) {
-        // Verwijdert de class verborgen
-        verborgenText.classList.remove('verborgen');
-        // Voegt de class showing-more toe en maakt de text zichtbaar
-        verborgenText.classList.add('showing-more');
-    } else if (verborgenText.classList.contains('showing-more')) {
-        // Verwijdert de class showing-more
-        verborgenText.classList.remove('showing-more');
-        // Voegt de class verborgen toe, dus de text verdwijnt      
-        verborgenText.classList.add('verborgen');
-    }
-});
 
